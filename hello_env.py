ea8c5b941f45a163f89f6f84ded5771140e29579
Alistair Miles <alimanfoo@googlemail.com>
2012-08-09 14:58:31 +0100

    remove env

--- hello_env.py
+++
@@ -1,5 +0,0 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    # expect to find DBSRV, DBUSER, DBPASS (name, user, passwd for sandbox database server)
-    for key, value in environ.items():
-        yield '%s: %s\n' % (key, value)

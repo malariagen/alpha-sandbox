7145c5271dc529e04dedde44656cf1ac0269354d
Paul Vauterin <paul.vauterin@gmail.com>
2012-08-30 14:27:42 +0100

    Update to correct server url

--- keposeda/Scripts/KPS_Build.js
+++ keposeda/Scripts/KPS_Build.js
@@ -116,8 +116,8 @@ $(function () {
     //Global initialisation of utilities
     DQX.Init();
 
-    theserverurl = "http://localhost:8000/app01";
-    //theserverurl = "/sandbox/keposeda/app";
+    //theserverurl = "http://localhost:8000/app01";
+    theserverurl = "/sandbox/keposeda/app";
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////
